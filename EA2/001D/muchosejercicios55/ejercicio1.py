7428034c32644b83675d4fed81fb9cb9583d37a9
leztdui <martincavero20@gmail.com>
2026-05-04 19:31:27 -0400

    Resuelto Ejercicio 1: Liquidación de Sueldo Básico

---
+++ EA2/001D/muchosejercicios55/ejercicio1.py
@@ -0,0 +1,10 @@
+Sueldo_base = 500000
+Bono_colacion =  50000
+bono_movilizacion = 30000
+
+Descuento_salud = Sueldo_base * 0.07
+
+Descuento_afp = Sueldo_base * 0.10
+
+sueldo_liquido = (Sueldo_base + Bono_colacion + bono_movilizacion)-(Descuento_salud + Descuento_afp)
+print(sueldo_liquido)
