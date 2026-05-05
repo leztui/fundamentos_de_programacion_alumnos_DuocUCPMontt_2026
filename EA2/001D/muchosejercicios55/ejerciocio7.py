3c568aebf3f1a0014199157f57afc26f8a596b8d
leztdui <martincavero20@gmail.com>
2026-05-04 21:06:02 -0400

    Resuelto Ejercicio 7: Armando el PC Gamer

---
+++ EA2/001D/muchosejercicios55/ejerciocio7.py
@@ -0,0 +1,11 @@
+procesador = 250000
+placa_madre = 120000
+tarjeta_video = 450000
+descuento = 0.15
+valor_total = 0 
+
+oferta = tarjeta_video *(1 - descuento)
+
+valor_total  = oferta + procesador + placa_madre
+
+print(valor_total)
