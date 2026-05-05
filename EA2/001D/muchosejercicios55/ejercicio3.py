1180a8ad32f506d25dad69e380f6c54189baee74
leztdui <martincavero20@gmail.com>
2026-05-04 20:05:46 -0400

    Resuelto Ejercicio 3: Repartiendo la Cuenta en el Bar

---
+++ EA2/001D/muchosejercicios55/ejercicio3.py
@@ -0,0 +1,13 @@
+personas = 5 
+pichanga = 15000 * 2 
+schops = 3500 * 5 
+boleta = (pichanga + schops)
+propina = 4750
+
+print(f"Su total a pagar es {boleta+propina}")
+total_pagar = (boleta + propina) /personas
+print(f"entre las 5 personas se repartiria en {total_pagar} ")
+
+
+
+
