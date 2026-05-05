78959ff0ea337a930d8950562aafde617e60fe9a
leztdui <martincavero20@gmail.com>
2026-05-04 20:48:31 -0400

    Resuelto Ejercicio 6: Ganancia en Criptomonedas (Binance)

---
+++ EA2/001D/muchosejercicios55/ejercicio6.py
@@ -0,0 +1,8 @@
+compra = 0.05
+btc_antiguo =  50000    
+btc_nuevo =  62000
+Dolar = 900 
+precio_total_btc= btc_nuevo - btc_antiguo
+tus_btc  = precio_total_btc * compra
+precio_btc= tus_btc * 900
+print(f" tus BTC atuales es de {precio_btc}")
