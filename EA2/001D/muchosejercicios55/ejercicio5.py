51f5349dc58d44cde2096efb151b71500c9d20c7
leztdui <martincavero20@gmail.com>
2026-05-04 20:20:22 -0400

    Resuelto Ejercicio 5: Administrador de Carga Bip! Avanzado

---
+++ EA2/001D/muchosejercicios55/ejercicio5.py
@@ -0,0 +1,10 @@
+Tarjeta =  10000
+micro = 730
+metro = 850
+recarga= 3000
+Tarjeta = Tarjeta - micro
+Tarjeta  = Tarjeta - metro
+Tarjeta = Tarjeta - metro 
+
+Tarjeta = Tarjeta + recarga
+print(f"el saldo de su trajeta despues de los 3 dias es {Tarjeta}")
